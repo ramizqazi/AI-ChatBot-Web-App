906f61e3431278051244abfe43d04b416930be96
ramizqazi <ramizqazi03@gmail.com>
2024-02-01 18:50:36 +0500

    add chat bot ui

--- src/containers/Chat/index.jsx
+++ src/containers/Chat/index.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, HStack, IconButton, Text } from '@chakra-ui/react';
+import { FaMicrophone } from 'react-icons/fa';
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from '@chatscope/chat-ui-kit-react';
 
 /* =============================================================================
 <Chat />
 ============================================================================= */
 const Chat = () => {
   return (
-    <Box w="full" h="full">
-      <Text>Hello World</Text>
+    <Box flex={1} pos="relative">
+      <ChatContainer>
+        <MessageList>
+          <Message
+            model={{
+              message: 'Hello my friend',
+              sentTime: 'just now',
+              sender: 'Joe',
+            }}
+          />
+        </MessageList>
+        <MessageInput placeholder="Type message here" attachButton={false} />
+      </ChatContainer>
+      <IconButton
+        icon={<FaMicrophone />}
+        pos="absolute"
+        bottom="5px"
+        left="-35px"
+        bg="transparent"
+      />
     </Box>
   );
 };
